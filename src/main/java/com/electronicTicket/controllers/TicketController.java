0fd065ea2cc9845b78720a08a334ad7d7e861629
szymon.kisiela <szymon.kisiela@asseco.pl>
2023-05-31 00:25:35 +0200

    feat: add authorization to AuthController and TicketController
    Add authorization to AuthController and TicketController by using the `@PreAuthorize` annotation with the `hasRole('ADMIN')` expression to restrict access to certain endpoints only to users with the 'ADMIN' role.

--- src/main/java/com/electronicTicket/controllers/TicketController.java
+++ src/main/java/com/electronicTicket/controllers/TicketController.java
@@ -6,6 +6,7 @@ import com.electronicTicket.repositories.TicketRepository;
 import com.electronicTicket.repositories.TicketTypeRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,7 @@ public class TicketController {
                 .map(ticketType -> modelMapper.map(ticketType, TicketTypeDto.class))
                 .toList();
     }
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/all")
     public List<TicketDto> getAllTickets() {
         return ticketRepository.findAll().stream()
